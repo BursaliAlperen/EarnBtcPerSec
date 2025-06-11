36db4667feed83347f57c0a7428fd74a62c93301
BursaliAlperen <bursali.alperen.131@gmail.com>
2025-06-11 18:27:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD35fhSe9gdyESPY8-6Pmn2mQZlj94z2XE",
+    authDomain: "earnbtcpersec.firebaseapp.com",
+    projectId: "earnbtcpersec",
+    storageBucket: "earnbtcpersec.firebasestorage.app",
+    messagingSenderId: "923239923427",
+    appId: "1:923239923427:web:81905b081546b648b50fd6",
+    measurementId: "G-FD2BS1JHGX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 import * as store from './store.js';
 import * as lang from './lang.js';
 
