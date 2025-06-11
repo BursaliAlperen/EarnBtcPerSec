49ff04ffae67b82c31fd5f4bee31d4317c8c030d
BursaliAlperen <bursali.alperen.131@gmail.com>
2025-06-11 19:03:03 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD35fhSe9gdyESPY8-6Pmn2mQZlj94z2XE",
+  authDomain: "earnbtcpersec.firebaseapp.com",
+  projectId: "earnbtcpersec",
+  storageBucket: "earnbtcpersec.firebasestorage.app",
+  messagingSenderId: "923239923427",
+  appId: "1:923239923427:web:81905b081546b648b50fd6",
+  measurementId: "G-FD2BS1JHGX"
+};
+
+// Firebase başlat
+const app = initializeApp(firebaseConfig);
+
+// Firestore'u al
+const db = getFirestore(app);
+
+export { db };
